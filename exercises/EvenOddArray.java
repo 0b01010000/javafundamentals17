0759b8a4902042ffd242a33fae7597b5e2d3ff5a
Taffarel Oliveira <202103371094@alunos.estacio.br>
2022-10-30 15:01:28 +0000

    add exercise: even odd array

---
+++ exercises/EvenOddArray.java
@@ -0,0 +1,30 @@
+package exercises;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class EvenOddArray {
+    public static void main(String[] args) {
+        // 2, 4, 8, 20, 1, 5, 7, 11
+        int[] numbers = {1, 2, 5, 4, 7, 8, 11, 20};
+
+        System.out.println(Arrays.toString(evenOddArray(numbers)));
+    }
+
+    private static int[] evenOddArray(int[] numbers) {
+        int size = numbers.length;
+        int[] nums = new int[size];
+
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (numbers[i] % 2 == 0) nums[j++] = numbers[i];
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (numbers[i] % 2 != 0) nums[j++] = numbers[i];
+        }
+
+        return nums;
+
+    }
+}
