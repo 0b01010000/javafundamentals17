dcaaee81093335296893dff0eb8d12a0771bc171
Taffarel Oliveira <115137992+0b01010000@users.noreply.github.com>
2022-11-06 15:56:49 -0300

    add example: `Date` class `@Deprecated`

---
+++ JavaLibraries.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class JavaLibraries {
+    public static void main(String[] args) {
+        System.out.println("Java Libraries!!!");
+        Date date = new Date();
+        
+        int hours = date.getHours();
+        System.out.println(date);
+        System.out.println(hours);
+    }
+}
